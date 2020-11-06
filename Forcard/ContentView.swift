ee0acf1a9efc5b0e7757acaba4f7f07c96b9662c
Dani <jomafica13.dp@gmail.com>
2020-11-06 15:33:52 +0000

    Initial Commit

---
+++ Forcard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Forcard
+//
+//  Created by Daniel Pereira on 06/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
